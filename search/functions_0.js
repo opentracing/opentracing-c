cf7fc365b7f4321caefebea53c7fda8f96f52123
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-07 13:16:18 +0000

    Deploy isaachier/opentracing-c to github.com/isaachier/opentracing-c.git:gh-pages

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['opentracing_5fglobal_5ftracer',['opentracing_global_tracer',['../tracer_8h.html#a4555099d3659ef4ecc6c7cbae67d7f43',1,'tracer.c']]],
-  ['opentracing_5finit_5fglobal_5ftracer',['opentracing_init_global_tracer',['../tracer_8h.html#a36c303a44e0916bb612ddb831ed717d9',1,'tracer.c']]]
+  ['opentracing_5fglobal_5ftracer',['opentracing_global_tracer',['../tracer_8h.html#a516db7dc68351c1c490c8c3aaa807586',1,'tracer.c']]],
+  ['opentracing_5finit_5fglobal_5ftracer',['opentracing_init_global_tracer',['../tracer_8h.html#ad2f773404dd0416fdda6ee93d484929a',1,'tracer.c']]]
 ];
