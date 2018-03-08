5ee383fe2357dd18859283ee25902e03558bed05
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-08 14:20:44 +0000

    Deploy isaachier/opentracing-c to github.com/isaachier/opentracing-c.git:gh-pages

--- search/all_c.js
+++ search/all_c.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['set',['set',['../structopentracing__text__map__writer.html#a836317c2b0562df5e11b648b85b45c38',1,'opentracing_text_map_writer']]],
-  ['set_5fbaggage_5fitem',['set_baggage_item',['../structopentracing__span.html#a0d73eb5304a3bcd79edc24715e0fa968',1,'opentracing_span']]],
-  ['set_5foperation_5fname',['set_operation_name',['../structopentracing__span.html#a931199e5ccc9537423b9a9be66407c80',1,'opentracing_span']]],
-  ['set_5ftag',['set_tag',['../structopentracing__span.html#a0d1128d3213f4f83573c03f768f9a8f5',1,'opentracing_span']]],
+  ['set_5fbaggage_5fitem',['set_baggage_item',['../structopentracing__span.html#a51869c036888ffecd110183a262acd92',1,'opentracing_span']]],
+  ['set_5foperation_5fname',['set_operation_name',['../structopentracing__span.html#a344bce42343b40c60c4d8b2ebe7054ef',1,'opentracing_span']]],
+  ['set_5ftag',['set_tag',['../structopentracing__span.html#ad20a96367509a57b9255cc2b4f620c00',1,'opentracing_span']]],
   ['span_2eh',['span.h',['../span_8h.html',1,'']]],
   ['span_5fcontext',['span_context',['../structopentracing__span.html#ae22872a3d49450d474abaa59aefbdc10',1,'opentracing_span']]],
   ['start_5fspan',['start_span',['../structopentracing__tracer.html#a9b8d103956aba278eb034f5dcbf32300',1,'opentracing_tracer']]],
