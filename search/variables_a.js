57d3bd0578ebc5b0dcb95ea4650f1a2f00fb1730
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-11 17:26:35 +0000

    Deploy isaachier/opentracing-c to github.com/isaachier/opentracing-c.git:gh-pages

--- search/variables_a.js
+++ search/variables_a.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['tags',['tags',['../structopentracing__start__span__options.html#ad6eded770a4eeb5eacc78fe6a56001f6',1,'opentracing_start_span_options']]],
   ['timestamp',['timestamp',['../structopentracing__log__record.html#aec648b3727e0b23307f52819615c606e',1,'opentracing_log_record']]],
-  ['tracer',['tracer',['../structopentracing__span.html#a2eb5254b57fa20934a3d48cb60e7dfb7',1,'opentracing_span']]],
+  ['tracer',['tracer',['../structopentracing__span.html#ad07f0089eaba25d6a01c9be8ec00f1ca',1,'opentracing_span']]],
   ['type',['type',['../structopentracing__value.html#a53609c679c5e5de98dadc29eba60c610',1,'opentracing_value']]]
 ];
